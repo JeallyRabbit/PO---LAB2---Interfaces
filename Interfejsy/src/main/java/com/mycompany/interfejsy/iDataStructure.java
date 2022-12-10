f780855463d864dd8d7e3bf6fbb039886cb3e1ba
JeallyRabbit <119430599+JeallyRabbit@users.noreply.github.com>
2022-12-10 17:30:00 +0100

    Add files via upload

---
+++ Interfejsy/src/main/java/com/mycompany/interfejsy/iDataStructure.java
@@ -0,0 +1,22 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.interfejsy;
+
+/**
+ *
+ * @author student
+ */
+public interface iDataStructure {
+    
+    void add(double newValue);
+    boolean cointains(double Searched);
+    double max();
+    double min();
+    void clear(double value);
+    void sort();
+    
+    
+    
+}
